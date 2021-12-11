8b86baedee4eef847fba6979ea17ad47566716de
Marcus Lassila <69545832+Marcus-Lassila@users.noreply.github.com>
2021-12-11 15:57:50 +0100

    Update solution.py

--- Day_10/solution.py
+++ Day_10/solution.py
@@ -7,7 +7,6 @@ illegalScoreTable = (3, 57, 1197, 25137)
 def solve(data):
     scoreOfIllegals = 0
     complementaries = []
-
     lp, rp = '([{<', ')]}>'
     mirror = dict(zip(rp + lp, lp + rp))
 
